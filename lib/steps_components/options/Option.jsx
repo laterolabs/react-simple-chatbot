c2be2324237149b12527f4fc76d80509747b79cd
Christopher Pang <christopher@laterolabs.com>
2020-11-13 10:00:52 -0800

    Changing scale(0) to scale(1)

--- lib/steps_components/options/Option.jsx
+++ lib/steps_components/options/Option.jsx
@@ -1,12 +1,10 @@
 import styled from 'styled-components';
-import { scale } from '../../common/animations';
 
 const Option = styled.li`
-  animation: ${scale} 0.3s ease forwards;
   cursor: pointer;
   display: inline-block;
   margin: 2px;
-  transform: scale(0);
+  transform: scale(1);
 `;
 
 export default Option;
